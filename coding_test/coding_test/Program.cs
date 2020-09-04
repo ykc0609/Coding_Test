f8fee5a8433c2551d1006648c18cabf46b5b17fb
周揚凱 <brian950703@gmail.com>
2020-09-03 19:57:19 -0700

    First version

---
+++ coding_test/coding_test/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+namespace coding_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //string[] input = System.IO.File.ReadAllLines("/Users/brian/Downloads/coding_test/coding_test/sample_test.txt");
+            // Read input from txt file
+            string[] input = System.IO.File.ReadAllLines("../../../sample_test.txt");
+
+            // Get the boundary
+            string[] size = input[0].Split(' ');
+            int GridLength = Int32.Parse(size[0]);
+            int GridWidth = Int32.Parse(size[1]);
+
+            // Using Hashtable to convert back the numeric direction to character
+            Hashtable DirectionInverseMap = new Hashtable() { { 0, 'N' }, { 1, 'E' }, { 2, 'S' }, { 3, 'W' } };
+
+            // Iterate through all the command. Assume that the input are always a initial information followed by a series command.
+            for (int row = 1; row < input.Length; row += 2)
+            {
+                string[] InitialInfo = input[row].Split(' ');
+                string command = input[row + 1];
+
+                // Construct a rover object 
+                Rover rover = new Rover();
+                // Set initial information
+                rover.SetFace(InitialInfo[2].ToCharArray()[0]);
+                rover.SetPosition(Int32.Parse(InitialInfo[0]), Int32.Parse(InitialInfo[1]));
+
+                // A flag indicates that the position is valid or not
+                int OutofBound = 0;
+
+                // Iterate through command
+                for(int index = 0; index < command.Length; index++)
+                {
+                    char signal = command[index];
+                    
+                    // Check if the current position is valid
+                    if(rover.CheckValid(GridLength, GridWidth))
+                    {
+                        Console.WriteLine("The rover is out of bound");
+                        OutofBound = 1;
+                        break;
+                    }
+
+                    // Assume the command is only consist of 'M','L' and 'R'.
+                    if (signal.Equals('M'))
+                    {
+                        rover.Walk();
+                        
+                    }
+                    // Rotate the head
+                    else
+                    {
+                    
+                        rover.Rotate(signal);
+                    }
+                }
+                // If all the command is valid, print the info.
+                if (OutofBound == 0)
+                {
+                    Console.WriteLine(rover.x.ToString() + ' ' + rover.y.ToString() + ' ' + DirectionInverseMap[rover.facing].ToString());
+                }
+                
+            }
+        }
+    }
+}
---
+++ coding_test/coding_test/Rover.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+namespace coding_test
+{
+    public class Rover
+    {
+        // Using Hashtable to transfer direction from character into numerical.
+        private Hashtable DirectionMap = new Hashtable() { { 'N', 0 }, { 'E', 1 }, { 'S', 2 }, { 'W', 3 } };
+        // Initial facing direction.
+        public int facing = -1;
+        // initial position.
+        public int x = 0;
+        public int y = 0;
+
+        // Set initial head 
+        public void SetFace(char direction)
+        {
+            this.facing = (int)DirectionMap[direction];
+        }
+
+        // Set initial position
+        public void SetPosition(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Rotate base on command
+        public void Rotate(char command)
+        {
+            switch (command)
+            {
+                case 'L':
+                    this.facing--;
+                    if (this.facing < 0)
+                    {
+                        this.facing += 4;
+                    }
+                    break;
+                case 'R':
+                    this.facing++;
+                    this.facing %= 4;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // Change the position base on facing direction
+        public void Walk()
+        {
+            switch (this.facing)
+            {
+                case 0:
+                    this.y++;
+                    break;
+                case 1:
+                    this.x++;
+                    break;
+                case 2:
+                    this.y--;
+                    break;
+                case 3:
+                    this.x--;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // Check if the current position is out of bound or not
+        public Boolean CheckValid(int x,int y)
+        {
+            if (this.x > x || this.x < 0 || this.y > y || this.y < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+    }
+}
